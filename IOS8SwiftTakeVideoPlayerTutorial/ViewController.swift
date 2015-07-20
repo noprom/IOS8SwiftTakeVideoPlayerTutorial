9183a8ec17a2433704316f56861bd44f402f9bfa
noprom <tyee.noprom@qq.com>
2015-07-20 23:19:22 +0800

    implemented takeVideo method

--- IOS8SwiftTakeVideoPlayerTutorial/ViewController.swift
+++ IOS8SwiftTakeVideoPlayerTutorial/ViewController.swift
@@ -7,9 +7,37 @@
 //
 
 import UIKit
+import MobileCoreServices
+import AssetsLibrary
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,UINavigationControllerDelegate, UIImagePickerControllerDelegate{
 
+    /**
+    摄像
+    
+    :param: sender <#sender description#>
+    */
+    @IBAction func takeVideo(sender: AnyObject) {
+        // 1.Check if project runs on a device with camera available
+        if UIImagePickerController.isSourceTypeAvailable(.Camera) {
+        
+            // 2.Present UIImagePickerController to take video
+            let controller = UIImagePickerController()
+            controller.sourceType = .Camera
+            controller.mediaTypes = [kUTTypeMovie as! String]
+            controller.delegate = self
+            controller.videoMaximumDuration = 10.0
+            
+            presentViewController(controller, animated: true, completion: nil)
+        }else{
+            println("Camera is not available")
+        }
+    }
+    
+    @IBAction func viewLibrary(sender: AnyObject) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
